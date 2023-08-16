cc42fffcbd72fc803378af4b0e73be37d1e017f4
Edmund Gröpl <114583103+groepl@users.noreply.github.com>
2023-08-16 12:06:23 +0200

    Add files via upload

---
+++ Assets/Tagging in Zettelkasten_v14.mm
@@ -0,0 +1,111 @@
+﻿<map version="0.9.0">
+	<node TEXT="How to Tag Notes in Zettelkasten">
+		<node TEXT="Tags for Subjects">
+			<node TEXT="#theme/"/>
+			<node TEXT="#index/"/>
+		</node>
+		<node TEXT="Tags for Types">
+			<node TEXT="#type/">
+				<node TEXT="book"/>
+				<node TEXT="chart"/>
+				<node TEXT="concept"/>
+				<node TEXT="example"/>
+				<node TEXT="exercise"/>
+				<node TEXT="insight"/>
+				<node TEXT="kanban"/>
+				<node TEXT="method"/>
+				<node TEXT="note"/>
+				<node TEXT="okr"/>
+				<node TEXT="person"/>
+				<node TEXT="question"/>
+				<node TEXT="quote"/>
+				<node TEXT="recipe"/>
+				<node TEXT="search"/>
+				<node TEXT="sketchnote"/>
+				<node TEXT="structure"/>
+				<node TEXT="term"/>
+				<node TEXT="tool"/>
+				<node TEXT="video"/>
+				<node TEXT="visual"/>
+				<node TEXT="..."/>
+			</node>
+		</node>
+		<node TEXT="Tags for Types with Attributes">
+			<node TEXT="#chart/">
+				<node TEXT="bar-chart"/>
+				<node TEXT="bubble-chart"/>
+				<node TEXT="cord-chart"/>
+				<node TEXT="..."/>
+			</node>
+			<node TEXT="#kanban/">
+				<node TEXT="1_backlog"/>
+				<node TEXT="2_todo"/>
+				<node TEXT="3_doing"/>
+				<node TEXT="4_review"/>
+				<node TEXT="5_done"/>
+			</node>
+			<node TEXT="#okr/">
+				<node TEXT="annual"/>
+				<node TEXT="idea"/>
+				<node TEXT="keyaction"/>
+				<node TEXT="keyresult"/>
+				<node TEXT="objective"/>
+				<node TEXT="wish"/>
+			</node>
+			<node TEXT="#role/">
+				<node TEXT="author"/>
+				<node TEXT="expert"/>
+				<node TEXT="friend"/>
+				<node TEXT="illustrator"/>
+				<node TEXT="sketchnoter"/>
+				<node TEXT="..."/>
+			</node>
+			<node TEXT="#structure/">
+				<node TEXT="bujo"/>
+				<node TEXT="faq"/>
+				<node TEXT="framework"/>
+				<node TEXT="glossary"/>
+				<node TEXT="list"/>
+				<node TEXT="mindmap"/>
+				<node TEXT="moc"/>
+				<node TEXT="toc"/>
+				<node TEXT="versus"/>
+				<node TEXT="workflow"/>
+				<node TEXT="..."/>
+			</node>
+			<node TEXT="#visual/">
+				<node TEXT="black"/>
+				<node TEXT="handlettering"/>
+				<node TEXT="yellow"/>
+				<node TEXT="..."/>
+			</node>
+		</node>
+		<node TEXT="Tags for Flow">
+			<node TEXT="#source/">
+				<node TEXT="article"/>
+				<node TEXT="book"/>
+				<node TEXT="internet"/>
+				<node TEXT="onenote"/>
+				<node TEXT="podcast"/>
+				<node TEXT="post"/>
+				<node TEXT="self"/>
+				<node TEXT="video"/>
+				<node TEXT="..."/>
+			</node>
+			<node TEXT="#target/">
+				<node TEXT="ebook"/>
+				<node TEXT="newsletter"/>
+				<node TEXT="discussion"/>
+				<node TEXT="meeting"/>
+				<node TEXT="post"/>
+				<node TEXT="presentation"/>
+				<node TEXT="..."/>
+			</node>
+		</node>
+	</node>
+	<node TEXT="Legend"/>
+	<node TEXT="used to measure productivity"/>
+	<node TEXT="mandatory tags"/>
+	<node TEXT="notes created with templates "/>
+	<node TEXT="Gröpl, Edmund. Make Useful Notes, 2023.  https://github.com/groepl/Take-Useful-Notes."/>
+</map>
